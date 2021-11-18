9a79d86ea086428277b4e0098869364ef354180f
Corentin Martin <corentin.martin@dynamicscreen.com>
2021-11-18 15:10:41 +0100

    message handler

--- src/AdvancedMessage/AdvancedMessageHandler.php
+++ src/AdvancedMessage/AdvancedMessageHandler.php
@@ -1,36 +1,18 @@
 <?php
 
-namespace DynamicScreen\Message\AdvancedMessage;
+namespace DynamicScreen\Message\SimpleMessage;
 
-use App\Domain\Module\Model\Module;
 use DynamicScreen\SdkPhp\Handlers\SlideHandler;
 use DynamicScreen\SdkPhp\Interfaces\ISlide;
-use JetBrains\PhpStorm\ArrayShape;
 
-class SimpleMessageHandler extends SlideHandler
+class AdvancedMessageHandler extends SlideHandler
 {
-    public function __construct(Module $module)
-    {
-        parent::__construct($module);
-    }
-
     public function fetch(ISlide $slide): void
     {
-        $default = $this->getDefaultOptions();
-
         $this->addSlide([
-            'title' => $slide->getOption('title', $default['title']),
-            'message' => $slide->getOption('message', $default['message']),
-            'backgroundColor' => $slide->getOption('backgroundColor', $default['color']),
+            'title' => $slide->getOption('title', ""),
+            'message' => $slide->getOption('message', ""),
+            'background_color' => $slide->getOption('background_color', "blue"),
         ]);
-    }
-
-    public function getDefaultOptions(): array
-    {
-        return [
-            'title' => '',
-            'message' => '',
-            'backgroundColor' => '#D42500',
-        ];
     }
 }
--- src/SimpleMessage/SimpleMessageHandler.php
+++ src/SimpleMessage/SimpleMessageHandler.php
@@ -2,34 +2,18 @@
 
 namespace DynamicScreen\Message\SimpleMessage;
 
-use App\Domain\Module\Model\Module;
 use DynamicScreen\SdkPhp\Handlers\SlideHandler;
 use DynamicScreen\SdkPhp\Interfaces\ISlide;
 
 class SimpleMessageHandler extends SlideHandler
 {
-    public function __construct(Module $module)
-    {
-        parent::__construct($module);
-    }
 
     public function fetch(ISlide $slide): void
     {
-        $default = $this->getDefaultOptions();
-
         $this->addSlide([
-            'title' => $slide->getOption('title', $default['title']),
-            'message' => $slide->getOption('message', $default['message']),
-            'background_color' => $slide->getOption('background_color', $default['background_color']),
+            'title' => $slide->getOption('title', ""),
+            'message' => $slide->getOption('message', ""),
+            'background_color' => $slide->getOption('background_color', "blue"),
         ]);
-    }
-
-    public function getDefaultOptions(): array
-    {
-        return [
-            'title' => '',
-            'message' => '',
-            'background_color' => 'blue'
-        ];
     }
 }
